6dd24f76f34d52f41fc74bd3f4c961bf2bd8d9e2
maysam <kherad@gmail.com>
2019-03-03 23:50:10 +0330

    first page

--- eee/App.js
+++ eee/App.js
@@ -1,21 +1,94 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import { Font, LinearGradient } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      proxima: require('./assets/fonts/proxima.ttf'),
+    });
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
+    if (!this.state.fontLoaded) return null;
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View
+        style={{
+          height: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+          }}>
+          <Image
+            source={require('./assets/images/background.png')}
+            style={{
+              position: 'absolute',
+              width: '100%',
+              height: '100%',
+              flex: 1,
+            }}
+          />
+          <LinearGradient
+            colors={['transparent', 'rgba(0,0,0,1)']}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+            }}
+          />
+        </View>
+        <View
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: '55%',
+            width: '100%',
+            borderRadius: 4,
+            borderWidth: 0,
+            borderColor: 'red',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Image
+            source={require('./assets/images/amazingco-logo.png')}
+            resizeMode={'contain'}
+            style={{
+              borderRadius: 4,
+              borderWidth: 0,
+              borderColor: 'blue',
+              height: 80,
+            }}
+          />
+          <Text
+            style={{
+              fontFamily: 'proxima',
+              flex: 1,
+              color: 'white',
+              borderRadius: 4,
+              borderWidth: 0,
+              borderColor: 'green',
+              fontSize: 20,
+              fontWeight: 'bold',
+            }}>
+            Time better spent
+          </Text>
+        </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
